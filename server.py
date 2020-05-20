e667423793f90181fdf5874a0059ad3314015820
saquib-rashid-msft <srashid@microsoft.com>
2020-05-20 17:29:50 -0400

    updated solution to use dotenv and k8s secrets

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from aiohttp import web
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 async def handle(request):      
     name = request.match_info.get('name', "World!")
     text = "Hello, " + name
+    secret = os.getenv('SECRET_MESSAGE')
+    fulltext = text + "\nSecret: " + secret + " (shhhhh)"
     print('received request, replying with "{}".'.format(text))
-    return web.Response(text=text)
+    return web.Response(text=fulltext)
 
 app = web.Application()
 app.router.add_get('/', handle)
